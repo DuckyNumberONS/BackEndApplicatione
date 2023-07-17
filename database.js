9a3aafdeb69b1108c01f1c2e9bd19e2c5d2df8b6
Hoang Minh Duc <duchm.bhsoft@gmail.com>
2023-07-17 11:12:19 +0700

    fix/setup database

--- database.js
+++ database.js
@@ -2,9 +2,6 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "Duc@nh2121",
-  database: "MoviesApp",
 });
 
 connection.connect((err) => {
